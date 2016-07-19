8a11610164f04339e3626163d649619c9c5dcdc7
David Montecillo <montecillodavid>
2016-07-20 01:12:35 +0800

    test

--- app/src/main/java/com/spurdow/circleviewtest/MainActivity.java
+++ app/src/main/java/com/spurdow/circleviewtest/MainActivity.java
@@ -3,11 +3,17 @@ package com.spurdow.circleviewtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spurdow.simplecircleview.SimpleCircleView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleCircleView simpleCircleView = (SimpleCircleView) findViewById(R.id.circleView);
+
+        simpleCircleView.setRadius(150);
     }
 }
