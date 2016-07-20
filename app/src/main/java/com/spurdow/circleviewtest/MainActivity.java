ee484c90992ea46cee9bb79864e4799eaaf13880
DavidMonts <montecillodavid.spingine@gmail.com>
2016-07-20 09:52:15 +0800

    added seekbar

--- app/src/main/java/com/spurdow/circleviewtest/MainActivity.java
+++ app/src/main/java/com/spurdow/circleviewtest/MainActivity.java
@@ -1,19 +1,42 @@
 package com.spurdow.circleviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.SeekBar;
 
 import com.spurdow.simplecircleview.SimpleCircleView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
+    SeekBar mSeekBar;
+    SimpleCircleView mSimpleCircleView;
+    static final int DEFAULT_PROGRESS = 25;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SimpleCircleView simpleCircleView = (SimpleCircleView) findViewById(R.id.circleView);
+        mSimpleCircleView = (SimpleCircleView) findViewById(R.id.circleView);
+        mSimpleCircleView .setRadius(DEFAULT_PROGRESS);
 
-        simpleCircleView.setRadius(150);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mSeekBar.setProgress(DEFAULT_PROGRESS);
+        mSeekBar.setMax(1000);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mSimpleCircleView.setRadius(progress);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
